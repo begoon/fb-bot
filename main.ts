a0b339e93bd588491d3d2158b1290cffb24a9106
Alexander Demin <ademin@gmail.com>
2024-09-04 20:24:59 +0200

    Add webhook verification

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import { env } from "node:process";
 
 const PORT = env.PORT || 8000;
+const FB_VERIFY_TOKEN = env.FB_VERIFY_TOKEN;
+console.log({ PORT, FB_VERIFY_TOKEN });
 
 Deno.serve(
     {
@@ -9,6 +11,22 @@ Deno.serve(
     },
     (request: Request) => {
         console.log(request.method, request.url);
+        const url = new URL(request.url);
+        const params = url.searchParams;
+        const path = url.pathname;
+        if (path === "/callback") {
+            const mode = params.get("hub.mode");
+            const token = params.get("hub.verify_token");
+            const challenge = params.get("hub.challenge");
+            console.log({ mode, token, challenge });
+            if (mode === "subscribe" && token === FB_VERIFY_TOKEN) {
+                console.log("callback verified");
+                return new Response(challenge);
+            } else {
+                console.log("callback failed");
+                return new Response("failed", { status: 403 });
+            }
+        }
         return new Response("OK");
     }
 );
